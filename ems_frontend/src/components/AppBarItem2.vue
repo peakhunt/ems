97f1098ec00a11ac230859ba1727ef24667c0c00
Huckleberry Kim <hawk.kim1974@gmail.com>
2021-11-25 16:20:12 +0900

    some bug fixed and a bunch of beautifies

---
+++ ems_frontend/src/components/AppBarItem2.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-hover v-slot="{ hover }">
+    <v-list-item
+     v-on="$listeners"
+     v-bind="$attrs"
+     :class="hover ? 'white--text primary elevation-12' : 'black--text'"
+     link
+    >
+      <slot name="content">
+      </slot>
+    </v-list-item>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  name: 'AppBarItem2',
+};
+</script>
